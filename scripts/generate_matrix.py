95409f92f573d2617e6a409c8e69dc19e84f043f
Mischa Lieser <mischa.lieser@gmail.com>
2025-12-20 07:43:37 +0100

    dongle build

--- scripts/generate_matrix.py
+++ scripts/generate_matrix.py
@@ -16,7 +16,7 @@ format_shields = {
 
 groups = []
 for keymap in keymaps:
-    for fmt in ["bt", "dongle"]:
+    for fmt in ["bt"]:
         groups.append({
             "keymap": keymap,
             "format": fmt,
